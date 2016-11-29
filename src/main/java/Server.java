fe49108547c81543fc32124a7fe75706e925dd4b
Oleksandr <avpavlov96@gmail.com>
2016-11-29 16:05:47 +0300

    fields

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,6 +7,15 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Server {
+public class Server extends JFrame {
+
+    private JTextField userText;
+    private JTextArea chatWindow;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private ServerSocket server;
+    private Socket connection;
+
+    
 
 }
